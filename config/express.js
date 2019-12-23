ec31e29feb4fcb0058093517e97c91055921f070
nhath <nhath@nhaths-Thinkpad.local>
2019-12-23 17:35:52 +0700

    fix

--- config/express.js
+++ config/express.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var router = require('./router')
 var app = express();
 var bodyParser = require('body-parser');
 var passport = require('./passport')
@@ -11,12 +10,9 @@ var passport = require('./passport')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// view engine setup
 app.use(logger('dev'));
-
 app.use(passport.initialize())
 
-router(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -28,7 +24,6 @@ app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-
     // render the error page
     res.status(err.status || 500).json(err);
 });
